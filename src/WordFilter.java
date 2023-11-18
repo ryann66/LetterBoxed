54c2227e6a225e8233003ce91ae2c5aff57f6a02
ryann66 <ryannelson6@hotmail.com>
2023-11-18 12:09:18 -0800

    Added WordFilter template

---
+++ src/WordFilter.java
@@ -0,0 +1,19 @@
+public class WordFilter {
+    /**
+     * Creates a new WordFilter for LetterBoxed
+     * @param edges an array of edges where each edge is an array of the
+     *              characters on that edge
+     */
+    public WordFilter (Character[][] edges) {
+
+    }
+
+    /**
+     * Checks if the word passes the filter
+     * @param word the word to check
+     * @return true if passes, else false
+     */
+    public boolean Valid(String word) {
+        return false;
+    }
+}
